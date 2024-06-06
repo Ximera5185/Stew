850c9f40650ac970528c9597b0e75d262d7786dd
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-06 08:46:14 +0300

    1

--- Stew/Database.cs
+++ Stew/Database.cs
@@ -12,9 +12,22 @@ namespace Stew
 
         public Database() 
         {
-        
+            AddStaws();
         }
 
-        private void AddStaws() { }
+        private void AddStaws() 
+        {
+            _stews.Add(new Stew(1989,10));
+            _stews.Add(new Stew(1990, 11));
+            _stews.Add(new Stew(1995, 12));
+            _stews.Add(new Stew(2000, 10));
+            _stews.Add(new Stew(2001, 9));
+            _stews.Add(new Stew(2004, 15));
+            _stews.Add(new Stew(2007, 16));
+            _stews.Add(new Stew(2002, 13));
+            _stews.Add(new Stew(2003, 5));
+            _stews.Add(new Stew(2008, 12));
+            _stews.Add(new Stew(2024, 12));
+        }
     }
 }
