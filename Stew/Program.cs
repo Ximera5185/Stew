3f691429d680d22c0151ee656760cb187c4f37f4
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-06 08:35:12 +0300

    Добавьте файлы проекта.

---
+++ Stew/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stew
+{
+    internal class Program
+    {
+        static void Main(string [] args)
+        {
+        }
+    }
+}
