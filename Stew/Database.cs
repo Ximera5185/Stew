9cea06bf114687e7f7d890f8703dca3c990d8e69
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-06 08:50:28 +0300

    1

--- Stew/Database.cs
+++ Stew/Database.cs
@@ -23,7 +23,7 @@ namespace Stew
             _stews.Add(new Stew("Тушонка Русская", 2000, 10));
             _stews.Add(new Stew("Тушонка Русская", 2001, 9));
             _stews.Add(new Stew("Тушонка Украинская", 2004, 15));
-            _stews.Add(new Stew("Тушонка Украинска", 2007, 16));
+            _stews.Add(new Stew("Тушонка Украинская", 2007, 16));
             _stews.Add(new Stew("Тушонка Монгольска", 2002, 13));
             _stews.Add(new Stew("Тушонка Монгольска", 2003, 5));
             _stews.Add(new Stew("Тушонка Вкусная", 2008, 12));
