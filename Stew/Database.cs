a2a458661ebd9f8a95b8244691aa0a86ae43ecd0
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-06-06 08:41:38 +0300

    1

---
+++ Stew/Database.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stew
+{
+    internal class Database
+    {
+        List<Stew> _stews = new List<Stew>();
+
+        public Database() 
+        {
+        
+        }
+
+        private void AddStaws() { }
+    }
+}
---
+++ Stew/Stew.cs
@@ -0,0 +1,15 @@
+﻿namespace Stew
+{
+    internal class Stew
+    {
+        public Stew(int yearOfProduction, int shelfLife)
+        {
+            YearOfProduction = yearOfProduction;
+
+            ShelfLife = shelfLife;
+        }
+
+        public int YearOfProduction { get; private set; }
+        public int ShelfLife { get; private set; }
+    }
+}
